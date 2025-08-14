418a580894ec66bcc8b46a2554a04b671727c3f3
gobi <gobixm@gmail.com>
2025-08-14 09:32:08 +0300

    binary tree postorder

---
+++ Leetcode/Algorithms/BinaryTreePostorderSolution.cs
@@ -0,0 +1,47 @@
+﻿namespace Leetcode.Algorithms;
+
+public sealed class BinaryTreePostorderSolution
+{
+    public IList<int> PostorderTraversal(TreeNode? root)
+    {
+        var result = new List<int>();
+        var stack = new Stack<TreeNode>();
+        if (root is null)
+        {
+            return [];
+        }
+        
+        stack.Push(root);
+        while (stack.Any())
+        {
+            var current = stack.Pop();
+            result.Add(current.val);
+
+            if (current.left is not null)
+            {
+                stack.Push(current.left);
+            }
+
+            if (current.right is not null)
+            {
+                stack.Push(current.right);
+            }
+        }
+        
+        result.Reverse();
+
+        return result;
+    }
+
+    
+    public class TreeNode {
+        public int val;
+        public TreeNode? left;
+        public TreeNode? right;
+        public TreeNode(int val=0, TreeNode? left=null, TreeNode? right=null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
