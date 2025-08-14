2a789e3188612cdb36c8e5e1bd3bcaa9efe799b2
gobi <gobixm@gmail.com>
2025-08-14 13:43:57 +0300

    excel sheet column number

---
+++ Leetcode/Algorithms/ExcelSheetColumnNumberSolution.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Xunit;
+
+namespace Leetcode.Algorithms;
+
+public sealed class ExcelSheetColumnNumberSolution
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        var result = new StringBuilder();
+        var num = columnNumber;
+        while (num > 0)
+        {
+            num -= 1;
+            var rem = num % 26;
+            result.Append(ToChar(rem));
+            num /= 26;
+        }
+
+        return string.Join("", result.ToString().Reverse());
+    }
+
+    private char ToChar(int num)
+    {
+        return (char)('A' + num);
+    }
+
+    [Theory]
+    [InlineData(1, "A")]
+    [InlineData(28, "AB")]
+    [InlineData(701, "ZY")]
+    public void ConvertToTitle_Test(int column, string title)
+    {
+        Assert.Equal(ConvertToTitle(column), title);
+    }
+}
