fd3a3526b26d12e26388a1e833ab15fa163ff195
gobi <gobixm@gmail.com>
2025-08-16 18:22:20 +0300

    happy number

---
+++ Leetcode/Algorithms/HappyNumberSolution.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Xunit;
+
+namespace Leetcode.Algorithms;
+
+public sealed class HappyNumberSolution
+{
+    public bool IsHappy(int n)
+    {
+        var current = n;
+        HashSet<int> seen = [];
+
+        while (!seen.Contains(current))
+        {
+            seen.Add(current);
+            current = GetSumOfSquares(current);
+        }
+
+        return current == 1;
+    }
+
+    private int GetSumOfSquares(int n)
+    {
+        var digits = n.ToString();
+        var sum = 0;
+        foreach (var digit in digits)
+        {
+            var value = int.Parse(digit.ToString());
+            sum += value * value;
+        }
+
+        return sum;
+    }
+
+    [Theory]
+    [InlineData(19, true)]
+    [InlineData(2, false)]
+    public void IsHappy_Test(int input, bool happy)
+    {
+        var result = IsHappy(input);
+        Assert.Equal(happy, result);
+    }
+}
